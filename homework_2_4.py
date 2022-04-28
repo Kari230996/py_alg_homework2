2380a6a0ebb99dfaa16e3e70e00d3ebc14229804
Karina <claro.claire1996@gmail.com>
2022-04-28 22:21:32 +0300

    name of variable "sum" changed to "sum_"

--- homework_2_4.py
+++ homework_2_4.py
@@ -10,10 +10,10 @@ n = int(input("Введите количество элементов (n): "))
 
 i = 0
 range_num = 1
-sum = 0
+sum_ = 0
 
 while i < n:
-    sum += range_num
+    sum_ += range_num
     range_num /= -2
     i += 1
-print(f"Cумма n элементов: {sum}")
+print(f"Cумма n элементов: {sum_}")
